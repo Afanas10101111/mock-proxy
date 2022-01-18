2b4888a39b1b3d725bf4427fe905920351329b04
Afanas10101111 <68496260+Afanas10101111@users.noreply.github.com>
2022-01-18 22:26:48 +0300

    [MP-0] Migrate on spring-cloud-gateway

---
+++ src/main/java/com/github/afanas10101111/mp/config/ProxyConfig.java
@@ -0,0 +1,30 @@
+package com.github.afanas10101111.mp.config;
+
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "proxy")
+class ProxyConfig {
+    public static final String ROUTE_ID = "main";
+
+    private String path;
+    private String url;
+
+    @Bean
+    RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(ROUTE_ID, route -> route
+                        .path(path)
+                        .and()
+                        .readBody(String.class, StringUtils::hasText)
+                        .uri(url))
+                .build();
+    }
+}
--- src/main/java/com/github/afanas10101111/mp/filter/PreFilter.java
+++ src/main/java/com/github/afanas10101111/mp/filter/PreFilter.java
@@ -1,55 +1,51 @@
 package com.github.afanas10101111.mp.filter;
 
 import com.github.afanas10101111.mp.service.RequestBodyChecker;
-import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StreamUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-
-import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class PreFilter extends ZuulFilter {
+public class PreFilter implements GlobalFilter {
+    private static final String CONTENT_TYPE = "text/xml;charset=UTF-8";
+    public static final String REQUEST_BODY_OBJECT = "cachedRequestBodyObject";
+
     private final RequestBodyChecker checker;
 
     @Override
-    public String filterType() {
-        return PRE_TYPE;
-    }
-
-    @Override
-    public int filterOrder() {
-        return 0;
-    }
-
-    @Override
-    public boolean shouldFilter() {
-        return true;
-    }
-
-    @Override
-    public Object run() {
-        RequestContext context = RequestContext.getCurrentContext();
-        try (InputStream in = context.getRequest().getInputStream()) {
-            String body = StreamUtils.copyToString(in, StandardCharsets.UTF_8);
-            log.info("run -> request: {}", body);
-            checker.getStubbedResponse(body).ifPresent(s -> {
-                log.info("run -> stubbed response: {}", s);
-                context.setResponseBody(s);
-                context.getResponse().setHeader("Content-Type", "text/xml;charset=UTF-8");
-                context.setSendZuulResponse(false);
-            });
-        } catch (IOException e) {
-            log.warn("run -> exception: {}", e.getMessage());
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String body = exchange.getAttribute(REQUEST_BODY_OBJECT);
+        if (body != null) {
+            log.info("filter -> request:\n{}", body);
+            Optional<String> stub = checker.getStubbedResponse(body);
+            if (stub.isPresent()) {
+                String stubString = stub.get();
+                log.info("filter -> stubbed response:\n{}", stubString);
+                ServerWebExchangeUtils.setResponseStatus(exchange, HttpStatus.OK);
+                ServerWebExchangeUtils.setAlreadyRouted(exchange);
+                return chain.filter(exchange).then(Mono.defer(() -> {
+                    ServerHttpResponse response = exchange.getResponse();
+                    response.getHeaders().add(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE);
+                    DataBuffer buffer = response.bufferFactory().wrap(stubString.getBytes());
+                    return response.writeWith(Flux.just(buffer));
+                }));
+            }
         }
-        return null;
+        log.info("filter -> redirected");
+        return chain.filter(exchange);
     }
 }
