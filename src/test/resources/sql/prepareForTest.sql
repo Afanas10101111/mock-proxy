320cf89e826b4ff7f55862dcad241c256e4dd1d1
Afanas10101111 <68496260+Afanas10101111@users.noreply.github.com>
2022-07-10 19:42:55 +0300

    [MP-8] Add the simplest version of the response delay

--- src/test/resources/sql/prepareForTest.sql
+++ src/test/resources/sql/prepareForTest.sql
@@ -3,10 +3,10 @@ FROM patterns;
 DELETE
 FROM rules;
 
-INSERT INTO rules (id, status, content_type, body, repeat_limit, repeat_counter)
-VALUES (1, 200, 'text/xml;charset=UTF-8', 'DEFAULT', 0, 0);
-INSERT INTO rules (id, status, content_type, body, repeat_limit, repeat_counter)
-VALUES (2, 200, 'text/xml;charset=UTF-8', 'SUBBED RESPONSE #1', 4, 0);
+INSERT INTO rules (id, status, content_type, body, delay, repeat_limit, repeat_counter)
+VALUES (1, 200, 'text/xml;charset=UTF-8', 'DEFAULT', 0, 0, 0);
+INSERT INTO rules (id, status, content_type, body, delay, repeat_limit, repeat_counter)
+VALUES (2, 200, 'text/xml;charset=UTF-8', 'SUBBED RESPONSE #1', 0, 4, 0);
 
 INSERT INTO patterns (id, pattern, mock_rule_id)
 VALUES (1, 'DEFAULT', 1);
